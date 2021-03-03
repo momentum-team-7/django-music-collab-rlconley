296dfbb0ef6c7a2e260a87612842194354d7df68
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-03-02 23:12:17 -0500

    add artist model and sample albums

--- core/models.py
+++ core/models.py
@@ -5,13 +5,20 @@ class User(AbstractUser):
     pass
 
 
-
 class Album(models.Model):
     title = models.CharField(max_length=280)
-    artist = models.CharField(max_length=280)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
     release_year = models.IntegerField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
 
     def __str__(self):
         return self.title
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=280)
+    label = models.CharField(max_length=280)
+
+    def __str__(self):
+        return self.name
 
