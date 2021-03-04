2f6e1ea64747feda3e12e2960cd8ab45bddc3a2c
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-03-04 12:07:12 -0500

    add login_required to homepage

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .forms import ArtistForm
 from .models import Album, Artist
 
 # Create your views here.
+@login_required
 def index(request):
     albums = Album.objects.filter(user=request.user)
     artists = Artist.objects.all()
